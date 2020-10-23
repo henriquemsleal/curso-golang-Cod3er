fdb2308dfc725979f6058acba5d1c925bbb7ea9f
henriquemsleal <henriquemsleal@gmail.com>
2020-10-23 15:56:26 -0300

    Atualizando aula for

--- controle/for/for.go
+++ controle/for/for.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	i := 1
@@ -8,4 +10,27 @@ func main() {
 		fmt.Println(i)
 		i++
 	}
+
+	for i := 0; i <= 20; i += 2 {
+		// 1a parte inicializa variável; 2a - condição; 3a - incremento
+		fmt.Printf("%d ", i)
+	}
+
+	fmt.Println("\nMisturando...")
+
+	// if em for
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Println(i, "é par")
+		} else {
+			fmt.Println(i, "é impar")
+		}
+	}
+
+	// laço infinito com for
+	/*
+		for {
+			fmt.Println("Para sempre...")
+			time.Sleep(time.Second)
+		}*/
 }
