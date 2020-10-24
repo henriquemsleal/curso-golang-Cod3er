3e445eab9e781280e91d7dbb56fcfb00149b30f9
henriquemsleal <henriquemsleal@gmail.com>
2020-10-24 16:31:00 -0300

    Aula array

---
+++ arrayslicemap/array/array.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// homogênea (mesmo tipo) e estática (fixo)
+	var notas [3]float64 // cria com valores zeros do tipo informado
+	fmt.Println(notas)
+
+	notas[0], notas[1], notas[2] = 7.8, 4.3, 9.1
+	fmt.Println(notas)
+
+	total := 0.0
+
+	for i := 0; i < len(notas); i++ {
+		total += notas[i]
+	}
+
+	media := total / float64(len(notas))
+	fmt.Println("A média das notas é", media)     // várias casas decimais
+	fmt.Printf("A média das notas é %.2f", media) // ponto flutuante com duas casas decimais
+}
